84c7efd1736283d180e43fb57af3177e281a5387
Merina Mou <merinamou7@gmail.com>
2025-12-26 19:19:57 +0600

    added a custom manager

--- apps/accounts/models.py
+++ apps/accounts/models.py
@@ -2,7 +2,33 @@
 User model - extends Django's AbstractUser
 """
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, BaseUserManager
+
+
+class UserManager(BaseUserManager):
+    """Custom user manager where email is the unique identifier"""
+    
+    def create_user(self, email, password=None, **extra_fields):
+        """Create and save a regular user with email and password"""
+        if not email:
+            raise ValueError('The Email field must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+    
+    def create_superuser(self, email, password=None, **extra_fields):
+        """Create and save a superuser with email and password"""
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+        
+        return self.create_user(email, password, **extra_fields)
 
 
 class User(AbstractUser):
@@ -31,6 +57,9 @@ class User(AbstractUser):
     # Use email as USERNAME_FIELD (allows username to be non-unique)
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []  # Email is the only required field for createsuperuser
+    
+    # Use custom manager
+    objects = UserManager()
     
     class Meta:
         ordering = ['-date_joined']
